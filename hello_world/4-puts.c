7596b7cd9de1df4e9c8655b57e8b2e74044d3668
OkanMahmudov <mahmudovokan47@gmail.com>
2025-02-27 18:06:11 +0400

    gshock

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string to the standard output
+ *
+ * Return: 0 upon successful completion
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
