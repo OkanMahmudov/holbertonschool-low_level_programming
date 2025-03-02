32140e79da6fb4558f11a066bdbeb76ddf613a8b
OkanMahmudov <mahmudovokan47@gmail.com>
2025-03-02 13:39:18 +0400

    gcc

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -2,16 +2,15 @@
 
 int main(void)
 {
-	/* main - Print digits from 0 to 9 */
+/* main - Print digits from 0 to 9 */
 	int i;
 
 	for (i = 0; i < 10; i++)
-	{
-		printf("%d", i);
-	}
-
-	/* Print a newline at the end */
+{
+	printf("%d", i);
+}
+/* Print a newline at the end */
 	printf("\n");
 
-	return (0);
+return (0);
 }
