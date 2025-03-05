b71f06270b1f998d0786068714a826dbbd7302b2
OkanMahmudov <mahmudovokan47@gmail.com>
2025-03-05 17:04:50 +0400

    main.h

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char c);
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
-#endif /* MAIN_H */
+    return (0);
+}
