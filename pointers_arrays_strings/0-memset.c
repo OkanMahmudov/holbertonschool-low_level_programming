a4d2798f705a7e6074b1755389f7cb4ac58882dc
OkanMahmudov <mahmudovokan47@gmail.com>
2025-03-14 18:47:52 +0400

    ss

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,14 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
-char *_memset(char *s, char b, unsigned int n);
-{
-	unsigned int i = 0;
+char *_memset(char *s, char b, unsigned int n) {
+    unsigned int i = 0;
 
-	while (1> n)
-	{
-		*(s + i) = b;
-		i++;
-	}
-	return(s);
+    while (i < n) {
+        *(s + i) = b;
+        i++;
+    }
+    return s;
 }
