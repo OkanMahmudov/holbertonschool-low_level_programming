78ae962e123eefb50116e168b6784b541e755c86
OkanMahmudov <mahmudovokan47@gmail.com>
2025-03-15 18:34:05 +0400

    dfsdgsg

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,37 +1,38 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * _sqrt_recursion - check the code
- * @n: square
- * Return: Always 0.
+ * sqrt_helper - helper function to find the square root.
+ * @n: the number for which to find the square root.
+ * @i: the current guess for the square root.
+ *
+ * Return: the square root if found, otherwise -1.
+ */
+int sqrt_helper(int n, int i)
+{
+	if (i * i == n)  /* Base case: if i^2 is equal to n, we've found the square root */
+	{
+		return (i);
+	}
+
+	if (i * i > n)  /* If i^2 is greater than n, no square root exists */
+	{
+		return (-1);
+	}
+
+	return (sqrt_helper(n, i + 1));  /* Recursively check the next integer */
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: the number to find the square root of.
+ *
+ * Return: the square root if it exists, otherwise -1.
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
-		return (-1);
+		return (-1);  /* Square root is not defined for negative numbers */
 	}
-	else if (n == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		return (_sqrt(n, 1));
-	}
-}
-/**
- * _sqrt - check the code
- * @x: x
- * @y: y
- * Return: Always 0.
- */
-int _sqrt(int y, int x)
-{
-	if (x * x == y)
-		return (x);
-	else if (x * x > y)
-		return (-1);
-	else
-		return (_sqrt(y, x + 1));
+	return (sqrt_helper(n, 0));  /* Start the recursion with i = 0 */
 }
