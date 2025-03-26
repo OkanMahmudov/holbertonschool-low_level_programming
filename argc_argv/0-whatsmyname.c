52a3d694145fd1636f69d18600b9f20e4fe7f774
OkanMahmudov <mahmudovokan47@gmail.com>
2025-03-26 12:30:38 +0400

    Betty styled

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 
 /**
- * main - prints the program name
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success
- */
+* main - prints the program name
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
-    (void)argc; /* Unused parameter */
-    printf("%s\n", argv[0]);
-    return (0);
+	(void)argc; /* Unused parameter */
+	printf("%s\n", argv[0]);
+	return (0);
 }
+
